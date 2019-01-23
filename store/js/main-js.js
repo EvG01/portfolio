4ccf57cd68cf6192331b0cfe21a3fceace2eb5bf
EvG01 <42220147+EvG01@users.noreply.github.com>
2019-01-23 23:09:53 +0200

    .

---
+++ store/js/main-js.js
@@ -0,0 +1,10 @@
+$( function() {
+    $('.for-b').hover( function(){
+        $("#for-b_list").slideToggle( 800 );
+    });
+    
+    $('.contacts').hover( function(){
+        $("#contacts_list").slideToggle( 800 );
+    });
+    
+});
