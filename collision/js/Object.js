c902912894abe1be0a07acd4cfe39e1f21058fc9
EvG01 <42220147+EvG01@users.noreply.github.com>
2021-11-10 13:15:50 +0200

    Add files via upload

---
+++ collision/js/Object.js
@@ -0,0 +1,134 @@
+export default class Object {
+    windowWidth;
+    windowHeight;
+    dx;
+    dy;
+    randVX;
+    randVY;
+    img;
+    x;
+    y;
+
+    constructor(w, h, speed, windowWidth, windowHeight) {
+        this.windowWidth = windowWidth;
+        this.windowHeight = windowHeight;
+        this.w = w;
+        this.h = h;
+        this.speed = speed;
+        this.setRandomPosition();
+        this.setInitialDirection();
+        this.randVX = Math.floor( Math.random() * 2) - 0.5;
+        this.randVY = Math.floor( Math.random() * 2) - 0.5;
+        this.createElem();
+    }
+
+    createElem() {
+        this.img = document.createElement("img");
+        this.img.setAttribute('src', "./img.png");
+        this.img.style.cssText = `
+            width: ${this.w}px;
+            height: ${this.h}px;
+            position: absolute;
+            left: ${this.x}px;
+            top: ${this.y}px;
+        `
+    }
+
+    setRandomPosition() {
+        this.x = Math.floor(Math.random() * ( this.windowWidth - this.w ));
+        this.y = Math.floor(Math.random() * ( this.windowHeight - this.h ));
+    }
+
+    setInitialDirection() {
+        this.dx = Math.floor( Math.random() * 2 );
+        this.dy = Math.floor( Math.random() * 2 );
+        this.dx === 0 ? this.dx = -1 : this.dx = 1;
+        this.dy === 0 ? this.dy = -1 : this.dy = 1;
+    }
+
+    checkEdge = () => {
+
+        if ( this.top < 0 ) {
+            this.dy *= -1;
+        }
+
+        if ( this.right > this.windowWidth ) {
+            this.dx *= -1;
+        }
+
+        if ( this.down > this.windowHeight ) {
+            this.dy *= -1;
+        }
+
+        if ( this.left < 0 ) {
+            this.dx *= -1;
+        }
+    }
+
+    checkCollision( item ) {
+        if (item !== this) {
+
+            let leftRight_Check = item.left <= this.left && this.left <= item.right ||
+                                  item.left <= this.right && this.right <= item.right;
+
+            let topDown_Check = item.top <= this.top && this.top <= item.down ||
+                                item.top <= this.down && this.down <= item.down;
+
+
+            if ( Math.abs(this.top - item.down) <= 5 &&  leftRight_Check) {
+
+                this.dy *= -1;
+            }
+
+            if ( Math.abs(this.right - item.left) <= 5 && topDown_Check) {
+
+                this.dx *= -1;
+            }
+
+            if ( Math.abs(this.down - item.top) <= 5 && leftRight_Check) {
+
+                this.dy *= -1;
+            }
+
+            if ( Math.abs(this.left - item.right) <= 5 && topDown_Check) {
+                this.dx *= -1;
+            }
+        }
+    }
+
+    updatePosition() {
+        this.img.style.left = `${this.x}px`;
+        this.img.style.top = `${this.y}px`;
+    }
+
+    moveElem() {
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
+        this.img.style.left = `${this.x}px`;
+        this.img.style.top = `${this.y}px`;
+        this.checkEdge();
+    }
+
+    get left() {
+        return this.x;
+    }
+
+    get top() {
+        return this.y;
+    }
+
+    get right() {
+        return this.x + this.w;
+    }
+
+    get down() {
+        return this.y + this.h;
+    }
+
+    get velocity() {
+        return {
+            x: this.dx * this.speed * this.randVX,
+            y: this.dy * this.speed * this.randVY
+        };
+    }
+}
---
+++ collision/js/javascript.js
@@ -0,0 +1,89 @@
+import Object from "./Object.js";
+
+const w = prompt('Ширина НЛО: ', "100");
+const h = prompt('Высока НЛО: ', "100");
+const count = prompt('Количество: ', "10");
+const speed = prompt('Скорость: ', "2");
+
+// if ( w == null && h == null && count == null && speed ) {
+//     w = prompt('Ширина НЛО: ', "100");
+//     h = prompt('Высока НЛО: ', "100");
+//     count = prompt('Количество: ', "10");
+//     speed = prompt('Скорость: ', "2");
+// }
+
+const windowWidth = document.documentElement.clientWidth,
+    windowHeight = document.documentElement.clientHeight,
+    container = document.querySelector(".container");
+
+let array = [],
+    needCheck = true;
+
+function checkSpawn() {
+    while (needCheck) {
+        needCheck = false;
+
+        for ( const mainObj of array ) {
+        for ( const secondObj of array ) {
+            if ( mainObj !== secondObj ) {
+                let left_topCorner = (secondObj.left <= mainObj.left && mainObj.left <= secondObj.right) &&
+                                     (secondObj.top <= mainObj.top && mainObj.top <= secondObj.down);
+
+                let right_topCorner = (secondObj.left <= mainObj.right && mainObj.right <= secondObj.right) &&
+                                      (secondObj.top <= mainObj.top && mainObj.top <= secondObj.down);
+
+                let left_downCorner = (secondObj.left <= mainObj.left && mainObj.left <= secondObj.right) &&
+                                      (secondObj.top <= mainObj.down && mainObj.down <= secondObj.down);
+
+                let right_downCorner = (secondObj.left <= mainObj.right && mainObj.right <= secondObj.right) &&
+                                       (secondObj.top <= mainObj.down && mainObj.down <= secondObj.down);
+
+                if ( left_topCorner || right_topCorner || left_downCorner || right_downCorner ) {
+
+                    mainObj.setRandomPosition();
+                    mainObj.updatePosition();
+                    console.log('Respawn');
+                    needCheck = true;
+                }
+            }
+        }
+        }
+    }
+}
+
+function createObjs (w, h, count, speed) {
+
+    for (let i = 0; i < count; i++) {
+        let obj = new Object(w, h, speed, windowWidth, windowHeight);
+        array.push(obj);
+        container.appendChild(obj.img);
+    }
+
+    checkSpawn();
+}
+
+function moveObj () {
+
+    for ( const obj of array ) {
+        obj.moveElem();
+    }
+}
+
+function checkCol() {
+
+    for ( const mainObj of array ) {
+        for ( const secondObj of array ) {
+            mainObj.checkCollision( secondObj );
+        }
+    }
+}
+
+function update() {
+    moveObj();
+    checkCol();
+    requestAnimationFrame(update);
+}
+
+
+createObjs(+w, +h, +count, +speed);
+update();
